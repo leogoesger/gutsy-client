fffaec8e5f074a59c5c08167b1a7175db7c08d22
Leo Q <leoq91@gmail.com>
2017-12-18 14:13:12 -0800

    ROCK-214 : climb card and login card edit

--- src/components/climb/ClimbCard.js
+++ src/components/climb/ClimbCard.js
@@ -16,12 +16,37 @@ export default class ClimbCard extends React.Component {
     this.setState({showMessage: false, message: ''});
   }
 
+  _renderLink(climb) {
+    return (
+      <span style={{fontSize: '12px'}}>
+        <Link to={`/regions/${climb.subarea.area.subregion.region.id}`}>{`${
+          climb.subarea.area.subregion.region.name
+        }`}</Link>
+        {'  >  '}
+        <Link to={`/subregions/${climb.subarea.area.subregion.id}`}>{`${
+          climb.subarea.area.subregion.name
+        }`}</Link>
+        {'  >  '}
+        <Link to={`/areas/${climb.subarea.area.id}`}>{`${
+          this.props.climb.subarea.area.name
+        }`}</Link>
+        {'  >  '}
+        <Link to={`/subareas/${climb.subarea.id}`}>{`${
+          this.props.climb.subarea.name
+        }`}</Link>
+      </span>
+    );
+  }
+
   _renderInfo() {
     if (this.props.climb) {
       const images = [{original: image}, {original: image}, {original: image}];
       return (
         <div className="row" style={styles.container}>
-          <div className="col-lg-6 col-md-6 col-xs-12" style={styles.image}>
+          <div
+            className="col-lg-6 col-md-6 col-sm-6 col-xs-12"
+            style={styles.image}
+          >
             <ImageGallery
               items={images}
               lazyLoad={true}
@@ -32,33 +57,25 @@ export default class ClimbCard extends React.Component {
               showBullets={true}
             />
           </div>
-          <div className="col-lg-5 col-md-5 col-xs-12" style={styles.info}>
-            <span style={{fontSize: '12px'}}>
-              <Link
-                to={`/regions/${this.props.climb.subarea.area.subregion.region
-                  .id}`}
-              >{`${this.props.climb.subarea.area.subregion.region.name}`}</Link>
-              {'  >  '}
-              <Link
-                to={`/subregions/${this.props.climb.subarea.area.subregion.id}`}
-              >{`${this.props.climb.subarea.area.subregion.name}`}</Link>
-              {'  >  '}
-              <Link to={`/areas/${this.props.climb.subarea.area.id}`}>{`${this
-                .props.climb.subarea.area.name}`}</Link>
-              {'  >  '}
-              <Link to={`/subareas/${this.props.climb.subarea.id}`}>{`${this
-                .props.climb.subarea.name}`}</Link>
-            </span>
-            <h2>
-              {this.props.climb.name}
-              <ReactStars
-                count={5}
-                value={4.5}
-                size={24}
-                color2={'#ffd700'}
-                edit={false}
-              />
-            </h2>
+          <div
+            className="col-lg-6 col-md-6 col-sm-6 col-xs-12"
+            style={styles.info}
+          >
+            {this._renderLink(this.props.climb)}
+
+            <div style={styles.title}>
+              <div>{this.props.climb.name}</div>
+              <div>{this.props.climb.grade}</div>
+            </div>
+
+            <ReactStars
+              count={5}
+              value={4.5}
+              size={24}
+              color2={'#ffd700'}
+              edit={false}
+            />
+            <h4>Description</h4>
             <p>{this.props.climb.description}</p>
           </div>
         </div>
@@ -87,7 +104,7 @@ ClimbCard.propTypes = {
 const styles = {
   tabsCard: {
     margin: '0 auto',
-    marginTop: '130px',
+    marginTop: '100px',
     marginBottom: '50px',
     borderRadius: '5px',
     padding: '60px 60px 60px 60px',
@@ -102,7 +119,13 @@ const styles = {
     boxShadow:
       '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   },
-  info: {},
+  info: {paddingLeft: '40px'},
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '20px',
+    width: '50%',
+  },
   refresh: {
     display: 'inline-block',
     position: 'relative',
